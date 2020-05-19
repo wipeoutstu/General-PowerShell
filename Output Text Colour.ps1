73b9fbb02707ca3274924ae47657cbc12514ae72
Stuart Woods <stuartwoods@outlook.com>
2020-05-19 20:48:47 +0100

    Create Output Text Colour.ps1

---
+++ Output Text Colour.ps1
@@ -0,0 +1,14 @@
+﻿
+Function Write-Col([String[]]$Text, [ConsoleColor[]]$Color,$BackColor) {
+    for ($i = 0; $i -lt $Text.Length; $i++) {
+        Write-Host $Text[$i] -Foreground $Color[$i] -BackgroundColor $BackColor[$i] -NoNewLine
+    }
+    Write-Host
+}
+
+$VMSwitch1 = "vSwitch 2"
+#Write-Col -Text "Creating", "`n`t vSphere Std", "`n`t`t Switch ", "`n`t`t`t $VMSwitch1`n" -Color Red,Green,Yellow,Cyan -BackColor Yellow,Cyan,Green,Red
+
+#Write-Col -Text "Creating", " vSphere Std", " Switch ", " $VMSwitch1 " -Color Cyan,Cyan,Cyan,Yellow -BackColor Blue,Blue,Blue,DarkGreen
+
+Write-Col -Text "Creating vSphere Std Switch ", " $VMSwitch1 " -Color Cyan,Yellow -BackColor Blue,DarkGreen
